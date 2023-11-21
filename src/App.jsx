619234854b3d191709a15d372f06bf737ee0d523
Bishop ukpai <bishopukpai0@gmail.com>
2023-11-21 21:30:22 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RegisterForm from './Components/Form'
+
+const App = () => {
+  return (
+   <RegisterForm/>
+  )
+}
+
+export default App
---
+++ src/Components/Form.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import '../App.css'
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add form submission logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegisterForm;
